1dd1d4cae8795fa3e0caab321e9497338444bb90
your_father <Dav.id.bazhe.nov7@gmail.com>
2023-05-11 22:07:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,87 @@
+import telebot
+import datetime
+import threading
+from telebot import types
+
+bot = telebot.TeleBot('6148192339:AAHYR-Er2NHMTgITNdfs448m9Gh8Pt1k91U')
+
+
+####################################################################################################################
+@bot.message_handler(commands=['start'])
+def start(message):
+    start_mess_1 = f'Здравствуйте, <b>{message.from_user.first_name}</b>, это помощник студента ЮФУ'
+    start_mess_2 = f'Я могу хранить все ваши <u>методические материалы</u>, напоминать о твоих <u>дедлайнах</u> и <u>долгах</u>'
+    start_mess_3 = f'Чтобы воспользоваться моими функциями, нажмите на кнопку <b>"Возможности"</b>'
+    bot.send_message(message.chat.id, start_mess_1, parse_mode='html')
+    bot.send_message(message.chat.id, start_mess_2, parse_mode='html')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    opp = types.KeyboardButton('Возможности')
+    markup.add(opp)
+    bot.send_message(message.chat.id, start_mess_3, parse_mode='html', reply_markup=markup)
+
+
+###################################################################################################################
+@bot.message_handler(commands=['addremind'])
+def reminder_message(message):
+    bot.send_message(message.chat.id, 'Введите название напоминания:')
+    bot.register_next_step_handler(message, set_reminder_name)
+def set_reminder_name(message, tmp = ''):
+    user_data = {}
+    if tmp!='':
+        user_data[message.chat.id] = {'reminder_name': tmp}
+    else:
+        user_data[message.chat.id] = {'reminder_name': message.text}
+    bot.send_message(message.chat.id,
+                     'Введите дату и время, когда вы хотите получить напоминание в формате ГГГГ-ММ-ДД чч:мм')
+    bot.register_next_step_handler(message, reminder_set, user_data)
+def reminder_set(message, user_data):
+    try:
+        reminder_time = datetime.datetime.strptime(message.text, '%Y-%m-%d %H:%M')
+        now = datetime.datetime.now()
+        delta = reminder_time - now
+        if delta.total_seconds() <= 0:
+            bot.send_message(message.chat.id, 'Эта дата уже прошла')
+        else:
+            reminder_name = user_data[message.chat.id]['reminder_name']
+            bot.send_message(message.chat.id,
+                             f'Напоминание "<b>{reminder_name}</b>" установлено на <u>{reminder_time}</u>',
+                             parse_mode='html')
+            reminder_timer = threading.Timer(delta.total_seconds(), send_reminder, [message.chat.id, reminder_name])
+            reminder_timer.start()
+    except ValueError:
+        bot.send_message(message.chat.id, 'Вы ввели неверный формат даты и времени, попробуйте ещё раз')
+        set_reminder_name(message, user_data[message.chat.id]['reminder_name'])
+def send_reminder(chat_id, reminder_name):
+    bot.send_message(chat_id, f'Напоминание:\n{reminder_name}!', parse_mode='html')
+#################################################################################################################
+
+@bot.message_handler()
+def get_user_text(message):
+    if message.text == 'Возможности':
+        mess_opp = f'Сохранить файл - <b>/savefile</b>\nПолучить файл - <b>/getfile</b>\nУзнать расписание - <b>/schedule</b>\nДобавить напоминание - <b>/addremind</b>'
+        bot.send_message(message.chat.id, mess_opp, parse_mode='html')
+    else:
+        err_mess = 'Я тебя не понимаю'
+        bot.send_message(message.chat.id, err_mess)
+
+
+#################################################################################################################
+# @bot.callback_query_handler(func=lambda call: True)
+# def ansopp(call):
+# if call.data == 'opp'
+
+
+# @bot.message_handler(commands=['help'])
+
+# @bot.message_handler(commands=['timetable'])
+# def timetable(message):
+#   if message.text == 'Узнать расписание':
+#       gr_mess = 'Укажите свою группу'
+#      bot.send_message(message.chat.id, gr_mess)
+
+
+# @bot.message_handler()
+# def get_user_text(message):
+
+
+bot.polling(none_stop=True)
